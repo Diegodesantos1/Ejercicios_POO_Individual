be1f64a79ff401cdbee6142c5bbb93a59a07e538
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-19 00:36:30 +0100

    funciona correctamente el ejercicio 1, fqalta añadirlo al main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from Clases import Catastrofe
-from Clases.Catastrofe import *
+from Clases.Catastrofe import Catastrofe_activada
 
 
 def eleccion_ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar: \n -->1: Catástrofe\n -->2: \n -->3: \n -->4: \n"))
     if variable == 1:
-        Catastrofe.ocurrir_catastrofe()
+        from Clases import ejercicio1
     elif variable == 2:
         from Clases import ejercicio2
     elif variable == 3:
         from Clases import ejercicio3
     elif variable == 4:
-        from Clases import ejercicio3
+        from Clases import ejercicio4
     else:
         eleccion_ejercicio()
 
