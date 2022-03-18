cf650f2092217edfaf8ba5fbdacb4bfd65d2270a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-18 23:44:44 +0100

    añadidos los prints aunque da error en el archivo

--- Clases/dia_siguiente.py
+++ Clases/dia_siguiente.py
@@ -1,12 +1,14 @@
-
 class Catastrofe:
     def __init__(self,ciudad,empresa,empleado):
         self.ciudad=ciudad
         self.empresa=empresa
         self.empleado=empleado
     def ocurrir_catastrofe(self):
-        txt= "{0} {1} {2}"
+        txt= "{0},{1},{2}"
         return txt.format(self.ciudad, self.empresa,self.empleado)
 A=Catastrofe("Nueva York","YooHoo!","Martin")
 B=Catastrofe("Nueva York","YooHoo!","Salim")
 C=Catastrofe("Los Ángeles","YooHoo!","Xing")
+print(A.ocurrir_catastrofe())
+print(B.ocurrir_catastrofe())
+print(C.ocurrir_catastrofe())
